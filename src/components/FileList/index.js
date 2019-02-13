24b265919c2dafaf498a566270b558ca9a0fe419
Felipe Marcon <felipeemarcon@gmail.com>
2019-02-12 22:52:25 -0200

    Frontend done

--- src/components/FileList/index.js
+++ src/components/FileList/index.js
@@ -4,32 +4,45 @@ import { MdCheckCircle, MdError, MdLink } from 'react-icons/md';
 
 import { Container, FileInfo, Preview } from './styles';
 
-const FileList = () => (
+const FileList = ({ files, onDelete }) => (
   <Container>
-    <li>
-      <FileInfo>
-        <Preview src="https://nodeuploadsystem.s3.amazonaws.com/98b46c20adabf0df1184d21323433a06-felipe.jpeg" />
+    {files.map(uploadedFile => (
+      <li key={uploadedFile.id}>
+        <FileInfo>
+          <Preview src={uploadedFile.preview} />
+          <div>
+            <strong>{uploadedFile.name}</strong>
+            <span>
+              {uploadedFile.readableSize} 
+              {!!uploadedFile.url && (
+                <button onClick={() => onDelete(uploadedFile.id)}>Excluir</button>
+                )}
+              </span>
+          </div>
+        </FileInfo>
         <div>
-          <strong>profile.png</strong>
-          <span>64kb <button onClick={() => {}}>Excluir</button></span>
+          { !uploadedFile.uploaded && !uploadedFile.error && (
+            <CircularProgressbar
+            styles={{
+              root: { width: 24 },
+              path: { stroke: '#7159c1' }
+            }}
+            strokeWidth={10}
+            percentage={uploadedFile.progress}
+            />
+          )}
+
+          {uploadedFile.url && (
+            <a href={uploadedFile.url} target="_blank" rel="noopener noreferrer">
+              <MdLink style={{ marginRight: 8 }} size={24} color="#222" />
+            </a>
+          )}
+          
+          { uploadedFile.uploaded && <MdCheckCircle size={24} color="#78e5d5" /> }
+          { uploadedFile.error && <MdError size={24} color="#e57878" /> }
         </div>
-      </FileInfo>
-      <div>
-        <CircularProgressbar
-          styles={{
-            root: { width: 24 },
-            path: { stroke: '#7159c1' }
-          }}
-          strokeWidth={10}
-          percentage={60}
-        />
-        <a href="https://nodeuploadsystem.s3.amazonaws.com/98b46c20adabf0df1184d21323433a06-felipe.jpeg" target="_blank" rel="noopener noreferrer">
-          <MdLink style={{ marginRight: 8 }} size={24} color="#222" />
-        </a>
-        <MdCheckCircle size={24} color="#78e5d5" />
-        <MdError size={24} color="#e57878" />
-      </div>
-    </li>
+      </li>
+    ))}
   </Container>
 );
 
